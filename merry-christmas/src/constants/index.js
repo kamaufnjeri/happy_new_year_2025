d36228a7366ef87ab4eca2d8c90a76d170867635
kamaufnjeri <kamaufnjeri2019@gmail.com>
2024-12-31 20:13:01 +0300

    happy new year

--- merry-christmas/src/constants/index.js
+++ merry-christmas/src/constants/index.js
@@ -5,4 +5,4 @@ export const SNOWFLAKEIMAGE = '/assets/snow_flake.png';
 
 export const Message = "May this New Year bring you an abundance of God's blessings, where your dreams come true, your heart finds fulfillment, and every day is filled with hope, joy, and success. Wishing you a year of growth, prosperity, and unending peace.";
  
- export const myWebsiteUrl = "https://happy-new-year-2025-3prg.onrender.com"
+ export const myWebsiteUrl = "https://happy-new-year-2025-3prg.onrender.com/"
